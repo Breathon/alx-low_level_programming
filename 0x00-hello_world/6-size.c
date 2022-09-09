cf84acc6afff1176b30fd37341ba7bb38c0efba4
Breathon <jossybreathon@yahoo.com>
2022-09-08 19:56:48 -0700

    C05

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return 0
+*/
+int main(void)
+{
+	char c:
+clear
+
+
+
+
+clear
+erm pi MDQP MwW	M4KQ3Gqe[,mW[ZQE<AWFMO"EQewfk"
+
+
+
+
