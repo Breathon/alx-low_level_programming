d0b283404880fe443410043413b7c8502c86868d
Breathon <jossybreathon@yahoo.com>
2022-09-08 20:28:48 -0700

    C06

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
-
+}
 
 
 
