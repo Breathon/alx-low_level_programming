fe200b550a479d0d22462ae52426033787ddd74b
Breathon <jossybreathon@yahoo.com>
2022-09-09 19:19:30 -0700

    T10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,19 +4,19 @@
  * main - prints all possible different combinations of two digits
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int n, m:
-
+int n, m;
 for (n = 48; n <= 56; n++)
 {
-for (m = 49; m,= 57: m++)
+for (m = 49; m <= 57; m++)
 {
 if (m > n)
 {
 putchar(n);
 putchar(m);
-if(n != 56 || m !=57)
+if(n != 56 || m != 57)
 {
 putchar(',');
 putchar(' ');
