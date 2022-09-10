e7121ee37ba8bf83df3d468c67d47dbf211ada7f
Breathon <jossybreathon@yahoo.com>
2022-09-09 21:08:26 -0700

    T12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-/*********
- * main - a simple program that outputs unordered
- * Description: using the main function
+/**
+ * main - prints all possible combinations of two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
