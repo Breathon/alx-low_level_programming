29491e206930b4b1115f96686e2aa6c41d6e07f0
Breathon <jossybreathon@yahoo.com>
2022-09-15 08:59:23 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,7 +23,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-if (month == 2 && day == 60
+if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
@@ -34,4 +34,4 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-
+i
