11cbcf346065bb0d4d3db8a6756f34e08444e954
Breathon <jossybreathon@yahoo.com>
2022-09-15 21:06:21 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,42 +7,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
 	int x, y = 0;
 	int space;
 
-	i = 1;
-	j = 1;
 	if (size > 0)
+{
+	space = size - y - 1;
+while (x < size)
+{
+if (space > x)
+{
+_putchar(' ');
+}
+else
 
-	{
-		while (i <= size)
-		while (y < size)
-		{
-			while (j <= size - i)
-			{
-				_putchar(' ');
-				j++;
-			}
-				j = 1;
-				while (j <= i)
-				space = size - y - 1;
-				while (x < size)
-				{
-					_putchar('#');
-					j++;
-					if (space > x)
-					{
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar('#');
+		_putchar('#');
 					}
 					x++;
 				}
-				_putchar('\n');
-				i++;
+				
 				y++;
 				x = 0;
 				_putchar ('\n');
