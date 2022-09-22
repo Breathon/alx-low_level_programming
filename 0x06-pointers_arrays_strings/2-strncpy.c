c426277287294cb0de7727f2e45af399eb79cf7a
Breathon <jossybreathon@yahoo.com>
2022-09-22 06:16:16 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,7 @@
  * the remainder of the destination string is filled with null bytes.
  * Works identically to the standard library function `strncpy`.
  * @dest: buffer storing the string copy
- * @dest: buffer storing the string copy
+ * @src:the source string
  * @n:max nummber of byte copied
  * Return: returns
  */
@@ -14,7 +14,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
@@ -23,5 +23,4 @@ char *_strncpy(char *dest, char *src, int n)
 
 	return (dest);
 }
-
 
