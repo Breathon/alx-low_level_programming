f8f00bf1b7381ebe504b8dde3a0d1ca22779c97b
Breathon <jossybreathon@yahoo.com>
2022-10-06 17:13:39 -0700

    task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAON_H
+#define MAIN_H
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -9,4 +9,3 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /*MAIN_H*/
-
