2f04f3799bb9499a89907596f5242c76b57492e4
Breathon <jossybreathon@yahoo.com>
2022-10-13 19:29:01 -0700

    tak 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,7 +21,7 @@ void print_c(va_list c)
  */
 void print_s(va_list s)
 {
-	char *str = va_arg(s, char *s);
+	char *str = va_arg(s, char *);
 
 	if (str == NULL)
 		str = "(nil)";
@@ -36,7 +36,7 @@ void print_s(va_list s)
  */
 void print_i(va_list i)
 {
-	printf("%d" va_arg(i, int));
+	printf("%d", va_arg(i, int));
 }
 /**
  * print_f - prints a float
@@ -44,31 +44,42 @@ void print_i(va_list i)
  *
  * Return: void
  */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of arguments typea passed to the function
+ *
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
 	print_t p[] = {
-		{"c" print_c},
-		{"s" print_s},
-		{"i" print_i},
-		{"f" print_f},
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
 		{NULL, NULL}
 	};
 	va_list valist;
-	char *seperator = "";
+	char *separator = "";
 
 	va_start(valist, format);
 	i = 0;
-	while (format && format);
+	while (format && format[i]);
 	{
 		j = 0;
 		while (p[j].t != NULL)
 		{
-			if (*p(p[j].t) === format[i])
+			if (*(p[j].t) == format[i])
 			{
-				printf("%s", seperator);
-				p[j].f(valist);	
-				seperator = ", ";
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
 				break;
 			}
 			j++;
@@ -78,4 +89,3 @@ void print_all(const char * const format, ...)
 	va_end(valist);
 	printf("\n");
 }
-
